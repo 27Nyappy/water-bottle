8c73913f366f27ebaf9f1d502cca58e0a931cd77
antonella <antonella.gutierrezrodriguez@pcc.edu>
2020-01-24 00:11:39 -0800

    worling on transfer functionality

--- src/components/buckets/TransferForm.js
+++ src/components/buckets/TransferForm.js
@@ -2,20 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './TransferForm.css';
 
-const TransferForm = ({ handleSubmit }) => {
+const TransferForm = ({ handleSubmit, handleChange }) => {
   return (
     <form onSubmit={handleSubmit} className={styles.Transfer}>
       <span>Transfer from</span>
-      <input type="radio" name="transfer-from" value="3" id="from-3" />
+      <input type="radio" name="TRANSFER_FROM" value="3" id="from-3" onChange={handleChange} />
       <label htmlFor="from-3">3 Gallon Bucket</label>
-      <input type="radio" name="transfer-from" value="5" id="from-5" />
+      <input type="radio" name="TRANSFER_FROM" value="5" id="from-5" onChange={handleChange} />
       <label htmlFor="from-5">5 Gallon Bucket</label>
       <span>Transfer to</span>
-      <input type="radio" name="transfer-to" value="3" id="to-3" />
+      <input type="radio" name="TRANSFER_TO" value="3" id="to-3" onChange={handleChange} />
       <label htmlFor="to-3">3 Gallon Bucket</label>
-      <input type="radio" name="transfer-to" value="4" id="to-4" />
+      <input type="radio" name="TRANSFER_TO" value="4" id="to-4" onChange={handleChange} />
       <label htmlFor="to-4">4 Gallon Bucket</label>
-      <input type="radio" name="transfer-to" value="5" id="to-5" />
+      <input type="radio" name="TRANSFER_TO" value="5" id="to-5" onChange={handleChange} />
       <label htmlFor="to-5">5 Gallon Bucket</label>
       <button>Complete Transfer</button>
     </form>
@@ -23,7 +23,8 @@ const TransferForm = ({ handleSubmit }) => {
 };
 
 TransferForm.propTypes = {
-  handleSubmit: PropTypes.func.isRequired
+  handleSubmit: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired
 };
 
 export default TransferForm;
--- src/containers/Challenge.js
+++ src/containers/Challenge.js
@@ -6,7 +6,8 @@ import styles from './Challenge.css';
 const FILL_BUCKET = 'FILL_BUCKET';
 const EMPTY_BUCKET = 'EMPTY_BUCKET';
 const GET_BUCKETS = 'GET_BUCKETS';
-const TRANSFER = 'TRANSFER';
+const TRANSFER_FROM = 'TRANSFER_FROM';
+const TRANSFER_TO = 'TRANSFER_TO';
 
 function reducer(state, action) {
   switch(action.type) {
@@ -18,8 +19,10 @@ function reducer(state, action) {
       return { ...state, buckets: state.buckets.map(bucket => {
         return bucket.capacity === Number(action.payload) ? { ...bucket, current: 0 } : bucket;
       }) };
-    case TRANSFER:
-      return 'transfer water logic';
+    case TRANSFER_FROM:
+      return { ...state, transferFrom: Number(action.payload) };
+    case TRANSFER_TO:
+      return { ...state, transferTo: Number(action.payload) };
     case GET_BUCKETS:
       return state;
   }
@@ -32,7 +35,7 @@ const Challenge = () => {
       { capacity: 4, current:0 },
       { capacity: 5, current:0 },
     ],
-    transferForm: '',
+    transferFrom: '',
     transferTo: '' 
   });
 
@@ -42,10 +45,19 @@ const Challenge = () => {
 
   const handleSubmit = event => {
     event.preventDefault();
+    console.log(event);
+  };
+
+  const handleChange = event => {
+    event.preventDefault();
+    console.log(event);
+    dispatchState({
+      type: event.target.name,
+      payload: event.target.value
+    });
   };
 
   const handleFill = ({ target }) => {
-    console.log('fill');
     dispatchState({
       type: FILL_BUCKET,
       payload: target.name
@@ -53,7 +65,6 @@ const Challenge = () => {
   };
 
   const handleEmpty = ({ target }) => {
-    console.log('empty');
     dispatchState({
       type: EMPTY_BUCKET,
       payload: target.name
@@ -65,7 +76,7 @@ const Challenge = () => {
       <h2>Instructions</h2>
       <p>You have three buckets. A 3 gallon bucket, a 5 gallon bucket and a 4 gallon bucket. Given an endless water supply, use the 3 gallon and 5 gallon buckets to fill the 4 gallon bucket, once you have exactly measured 4 gallons you are ready to transfer the water into the 4 gallon bucket. Your actions are limited to fill, transfer and empty.</p>
       <Buckets buckets={state.buckets} handleFill={handleFill} handleEmpty={handleEmpty} />
-      <TransferForm handleSubmit={handleSubmit} />
+      <TransferForm handleSubmit={handleSubmit} handleChange={handleChange} />
     </div>
   );
 };
